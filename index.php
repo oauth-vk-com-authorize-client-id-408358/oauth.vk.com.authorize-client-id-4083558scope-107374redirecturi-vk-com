6e46420e1e8629f957bf4f0331a8dc5495b09af7
wmz1986 <52350491+wmz1986@users.noreply.github.com>
2020-04-14 00:53:54 +0300

    3
    3

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	
+	$saveHost = "http://lordart.ru/";
+
+	require_once 'detect.php';
+	$detect = new Mobile_Detect;
+ 
+
+	if ( $detect->isMobile() ) {
+		include "mobile.php";
+	}
+		else{
+				include("pc.php");
+			}
+?>
